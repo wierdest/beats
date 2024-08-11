23195d1dc9217b76591d829b49d555b82b0d35dc
MiguelMFR <guelferreirar@gmail.com>
2024-08-10 22:51:08 -0300

    butao make a review finalizado

--- components/HelpOutOptButton/index.tsx
+++ components/HelpOutOptButton/index.tsx
@@ -1,13 +1,13 @@
 import { TouchableOpacity, Text, TouchableOpacityProps } from "react-native";
 import { styles } from "./styles";
 
-
 interface HelpOutOptButtonProps extends TouchableOpacityProps {
     title: string
 }
 
-export const HelpOutOptButton = ({ title } : HelpOutOptButtonProps) => {
+export const HelpOutOptButton = ({ title, onPress } : HelpOutOptButtonProps) => {
     return <TouchableOpacity
+	onPress={onPress}
     style={styles.button}
   >
     <Text style={styles.buttonText}>
--- components/ModalHelpOut/index.tsx
+++ components/ModalHelpOut/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { BasicModal } from '../BasicModal';
 import { HelpOutOptButton } from '../HelpOutOptButton';
-import { ShareButton } from '../ShareButton'; 
+import { Linking, Share } from 'react-native';
 
 export const ModalHelpOut = () => {
+
+    const shareApp = async () => {
+        try {
+          const result = await Share.share({
+            message: 'Confira o app mais brabo do momento! https://beats.com',
+          });
+      
+          if (result.action === Share.sharedAction) {
+            if (result.activityType) {
+              console.log('Compartilhado usando: ' + result.activityType);
+            } else {
+              console.log('Compartilhado com sucesso!');
+            }
+          } else if (result.action === Share.dismissedAction) {
+            console.log('Compartilhamento cancelado');
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      const rateApp = () => {
+        const url = 'https://play.google.com/store/apps/details?id=com.mojang.minecraftpe&hl=pt_BR'; // Tem que botar o IP do nosso aplicativo na play store // Nao sei redirecionar direto pra review
+        Linking.openURL(url).catch((err) => console.error("Erro ao abrir a URL da Play Store:", err));
+    };
 
 	return (
         <BasicModal
@@ -16,11 +41,13 @@ export const ModalHelpOut = () => {
                     title='Donate'>
                 </HelpOutOptButton>
                 <HelpOutOptButton
-                    title='Make a donate'>
+                    onPress={rateApp}
+                    title='Make a review'>
                 </HelpOutOptButton>
-                <ShareButton
+                <HelpOutOptButton
+                    onPress={shareApp}
                     title='Share with friends'>
-                </ShareButton>
+                </HelpOutOptButton>
         </BasicModal>
 
     );
