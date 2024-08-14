85a6239ca6eeddb45ed3c085cb9946fbd97bdf26
MiguelMFR <guelferreirar@gmail.com>
2024-08-14 13:13:53 -0300

    alinhando Beat Name no centro

--- components/Player/styles.tsx
+++ components/Player/styles.tsx
@@ -6,28 +6,34 @@ export const createStyles = (isDarkMode: boolean) => {
             padding: 16,
             backgroundColor: isDarkMode ? '#1e1e1e' : 'lightgray',
             borderTopStartRadius: 30,
-            borderTopEndRadius: 30
+            borderTopEndRadius: 30,
         },
         topRow: {
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'space-between',
+            position: 'relative', // Para possibilitar a centralização absoluta do texto
+        },
+        centeredTextContainer: {
+            flex: 1,
+            alignItems: 'center', // Centraliza o texto horizontalmente
+            position: 'relative',
         },
         beatName: {
             fontSize: 36,
             fontWeight: 'bold',
             color: isDarkMode ? '#f5f5f5' : '#333',
+            position: 'absolute',
+            left: '50%',
+            transform: [{ translateX: "-50%" }],
         },
-        mainControls: {
-
-        },
+        mainControls: {},
         auxiliaryControls: {
             flexDirection: 'column',
             marginTop: 24,
-            
         },
         innerAuxControls: {
-            gap: 12
+            gap: 12,
         },
    
     });
