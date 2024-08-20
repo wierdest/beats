dfd6e8b25732ffae6ec96e34432dc96696c44997
andre <andrlzpt@protonmail.com>
2024-08-20 15:43:19 -0300

    updated test beats

--- beatsAssets.ts
+++ beatsAssets.ts
@@ -1,6 +1,15 @@
 
     const beatsAssetsMap: Record<string, any> = {
         '2bars': require('./assets/sounds/beats/2bars_other_120_80_240_44_2.mp3'),
+        'c-style1': require('./assets/sounds/beats/c-style1_hiphop_75_65_105_44_1.mp3'),
+        'c-style2': require('./assets/sounds/beats/c-style2_hiphop_75_65_105_44_1.mp3'),
+        'dark-phil': require('./assets/sounds/beats/dark-phil_hiphop_75_55_155_44_4.mp3'),
+        'ludwig': require('./assets/sounds/beats/ludwig_other_79_65_125_44_1.mp3'),
+        'queens': require('./assets/sounds/beats/queens_hiphop_79_65_125_44_1.mp3'),
+        'slick-d2': require('./assets/sounds/beats/slick-d2_hiphop_75_55_125_44_1.mp3'),
+        'slick-dLong': require('./assets/sounds/beats/slick-dLong_hiphop_75_55_145_44_2.mp3'),
+        'slick-d': require('./assets/sounds/beats/slick-d_hiphop_75_55_125_44_1.mp3'),
+        'slowburn': require('./assets/sounds/beats/slowburn_hiphop_72_64_144_44_1.mp3'),
     };
 
     export default beatsAssetsMap;
